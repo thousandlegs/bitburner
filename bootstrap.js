39d724e8c12d9dfc74e0c7f9822741c596b76041
Stephan A. Terre <7331433+thousandlegs@users.noreply.github.com>
2022-01-30 13:54:12 -0700

    Make bootstrap more bootstrappy

--- bootstrap.js
+++ bootstrap.js
@@ -4,6 +4,7 @@
 // you're either using Git exclusively or have manually captured anything
 // you care about
 const remote = "https://raw.githubusercontent.com/thousandlegs/bitburner/main/";
+const BOOTSTRAP = "bootstrap.js";
 const scripts = [
     "infect.js",
     "sesame.js",
@@ -11,12 +12,33 @@ const scripts = [
     "map_network.js",
     "purchase-servers.js",
     "gang_manager.js",
-    "bootstrap.js"
+    BOOTSTRAP
 ];
+
+
+async function _get(ns, fname) {
+    await ns.wget(remote + fname, fname);
+}
+
+async function update(ns) {
+    for (let ss of scripts) {
+        await _get(ns, ss);
+    }
+}
+
+async function bootstrap(ns) {
+    ns.tprint("Updating bootstrap itself");
+    await _get(ns, BOOTSTRAP);
+    ns.tprint("Updating all scripts");
+
+    ns.exec(BOOTSTRAP, "home");
+}
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    for (let ss of scripts) {
-        await ns.wget(remote + ss, ss);
+    if (ns.args[0] == "bootstrap") {
+        await bootstrap(ns);
+    } else {
+        await update(ns);
     }
 }
